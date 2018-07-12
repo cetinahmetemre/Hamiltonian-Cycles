999a21230ed35fbe581f14b26e38c6097ce5cdfa
unknown <warior_222@hotmail.com>
2018-07-12 17:16:26 +0300

    first commit

---
+++ hamiltoniancycles/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace hamiltoniancycles
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.metroTile1 = new MetroFramework.Controls.MetroTile();
+            this.edgesbutton = new MetroFramework.Controls.MetroTile();
+            this.vertexcountText = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.startButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.resetbutton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // metroTile1
+            // 
+            this.metroTile1.ActiveControl = null;
+            this.metroTile1.Location = new System.Drawing.Point(503, 24);
+            this.metroTile1.Name = "metroTile1";
+            this.metroTile1.Size = new System.Drawing.Size(107, 91);
+            this.metroTile1.TabIndex = 0;
+            this.metroTile1.Text = "Vertex";
+            this.metroTile1.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.metroTile1.TileImage = ((System.Drawing.Image)(resources.GetObject("metroTile1.TileImage")));
+            this.metroTile1.TileImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.metroTile1.UseSelectable = true;
+            this.metroTile1.UseTileImage = true;
+            this.metroTile1.Click += new System.EventHandler(this.vertexButton_Click);
+            // 
+            // edgesbutton
+            // 
+            this.edgesbutton.ActiveControl = null;
+            this.edgesbutton.Location = new System.Drawing.Point(681, 24);
+            this.edgesbutton.Name = "edgesbutton";
+            this.edgesbutton.Size = new System.Drawing.Size(107, 91);
+            this.edgesbutton.TabIndex = 1;
+            this.edgesbutton.Text = "Edges";
+            this.edgesbutton.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.edgesbutton.TileImage = ((System.Drawing.Image)(resources.GetObject("edgesbutton.TileImage")));
+            this.edgesbutton.TileImageAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.edgesbutton.UseSelectable = true;
+            this.edgesbutton.UseTileImage = true;
+            this.edgesbutton.Click += new System.EventHandler(this.edgesbutton_Click);
+            // 
+            // vertexcountText
+            // 
+            this.vertexcountText.Location = new System.Drawing.Point(257, 55);
+            this.vertexcountText.Name = "vertexcountText";
+            this.vertexcountText.Size = new System.Drawing.Size(100, 20);
+            this.vertexcountText.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(116, 58);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(135, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Please write vertex count : ";
+            // 
+            // startButton
+            // 
+            this.startButton.Location = new System.Drawing.Point(379, 53);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(75, 23);
+            this.startButton.TabIndex = 5;
+            this.startButton.Text = "Start";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(188, 543);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 6;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(63, 518);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(103, 63);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Find Hamiltonian Cycles";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // resetbutton
+            // 
+            this.resetbutton.Location = new System.Drawing.Point(841, 518);
+            this.resetbutton.Name = "resetbutton";
+            this.resetbutton.Size = new System.Drawing.Size(91, 63);
+            this.resetbutton.TabIndex = 8;
+            this.resetbutton.Text = "RESET";
+            this.resetbutton.UseVisualStyleBackColor = true;
+            this.resetbutton.Click += new System.EventHandler(this.resetbutton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(974, 635);
+            this.Controls.Add(this.resetbutton);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.vertexcountText);
+            this.Controls.Add(this.edgesbutton);
+            this.Controls.Add(this.metroTile1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private MetroFramework.Controls.MetroTile metroTile1;
+        private MetroFramework.Controls.MetroTile edgesbutton;
+        private System.Windows.Forms.TextBox vertexcountText;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button resetbutton;
+    }
+}
+
---
+++ hamiltoniancycles/Form1.cs
@@ -0,0 +1,273 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace hamiltoniancycles
+{
+    public partial class Form1 : Form
+    {
+        Label[] items;
+        int[,] vertexmatris;
+        int additem = 0, itemcount = 0, edgestack = 0, x1, y1, V = 0, edgescount=0;
+        string firstvertexname = "";
+        int[] path;
+        Pen[] p;
+        string[] edgesname;
+        Graphics g;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void vertexButton_Click(object sender, EventArgs e)
+        {
+            additem = 1;
+        }
+
+        private void edgesbutton_Click(object sender, EventArgs e)
+        {
+            additem = 2;
+        }
+
+        protected override void OnMouseDown(MouseEventArgs e)
+        {
+            base.OnMouseDown(e);
+
+            if (additem == 0);
+
+            if (additem == 1)
+                addVertex(e.X,e.Y);
+
+            if (additem == 2) ;
+            
+        }
+
+
+
+        private void addEdges(int x , int y , int x1 , int y1 , string firstvertexname , string secondvertexname)
+        {
+            g = CreateGraphics();
+            p[edgescount] = new Pen(Color.Black, 5);
+            g.DrawLine(p[edgescount],x,y,x1,y1);
+            edgesname[edgescount] = firstvertexname + secondvertexname;
+            additem = 0;
+            edgestack = 0;
+            g.Dispose();
+            edgescount++;
+            vertexmatris[Convert.ToInt16(firstvertexname), Convert.ToInt16(secondvertexname)] = 1;
+            vertexmatris[Convert.ToInt16(secondvertexname), Convert.ToInt16(firstvertexname)] = 1;
+        }
+
+
+        public void addVertex(int x , int y)
+        {
+            if (itemcount < V)
+            {
+                Image i = Image.FromFile(@"vertex.png");
+                items[itemcount] = new Label();
+                items[itemcount].Size = new System.Drawing.Size(53, 67);
+                items[itemcount].Location = new System.Drawing.Point(x, y);
+                items[itemcount].BackColor = System.Drawing.Color.Transparent;
+                items[itemcount].Name = itemcount+"";
+                items[itemcount].Image = i;
+                items[itemcount].Text = "" + itemcount;
+                items[itemcount].Font = new Font("Arial", 40, FontStyle.Bold);
+                items[itemcount].Click += new EventHandler(this.label_click);
+                this.Controls.Add(items[itemcount]);
+                additem = 0;
+                itemcount++;
+            }
+            else
+                MessageBox.Show("You have reached the maximum number");
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            if (vertexcountText.Text == "")
+                MessageBox.Show("Please write vertex count");
+            else
+            {
+                items = new Label[Convert.ToInt16(vertexcountText.Text)*5];
+                p = new Pen[Convert.ToInt16(vertexcountText.Text) * 5];
+                edgesname = new string[Convert.ToInt16(vertexcountText.Text) * 5];
+                
+                vertexmatris = new int[Convert.ToInt32(vertexcountText.Text), Convert.ToInt32(vertexcountText.Text)];
+                V = Convert.ToInt32(vertexcountText.Text);
+
+                for(int i = 0 ; i<Convert.ToInt32(vertexcountText.Text) ; i++)
+                {
+                    for (int j = 0; j < Convert.ToInt32(vertexcountText.Text); j++)
+                    {
+                        vertexmatris[i, j] = 0;
+                    }
+                }
+            }
+        }
+
+
+        private void label_click(object sender , EventArgs e)
+        {
+            Label label = sender as Label;
+
+            if(additem == 2)
+            {
+                if (edgestack == 0)
+                {
+                    x1 = label.Location.X;
+                    y1 = label.Location.Y;
+                    edgestack++;
+                    firstvertexname = label.Name;
+                }
+                else
+                    addEdges(x1, y1, label.Location.X, label.Location.Y, firstvertexname, label.Name);               
+
+                
+               
+           }
+        }
+
+
+            private bool isSafe(int v, int[,] graph, int[] path, int pos)
+            {
+                if (graph[path[pos - 1],v] == 0)
+                    return false;
+ 
+                for (int i = 0; i < pos; i++)
+                    if (path[i] == v)
+                        return false;
+ 
+                return true;
+            }
+
+            private bool hamCycleUtil(int[,] graph, int[] path, int pos)
+            {
+               
+                if (pos == V)
+                {
+                    
+                    if (graph[path[pos - 1],path[0]] == 1)
+                        return true;
+                    else
+                        return false;
+                }
+ 
+              
+                for (int v = 1; v < V; v++)
+                {
+                 
+                    if (isSafe(v, graph, path, pos))
+                    {
+                        path[pos] = v;
+ 
+                       
+                        if (hamCycleUtil(graph, path, pos + 1) == true)
+                            return true;
+ 
+                      
+                        path[pos] = -1;
+                    }
+                }
+ 
+              
+                return false;
+            }
+ 
+       
+        private int hamCycle(int[,] graph)
+        {
+            path = new int[V];
+            for (int i = 0; i < V; i++)
+                path[i] = -1;
+ 
+           
+            path[0] = 0;
+            if (hamCycleUtil(graph, path, 1) == false)
+            {
+                label1.Text="Solution does not exist";
+                return 0;
+            }
+ 
+            printSolution(path);
+            return 1;
+        }
+ 
+      
+        private void printSolution(int[] path)
+        {
+            label1.Text="Solution Exists: Following" +
+                               " is one Hamiltonian Cycle : ";
+            for (int i = 0; i < V; i++)
+            {
+                label1.Text += " " + path[i] + " ";
+
+                for(int j = 0 ; j < edgescount ; j++)
+                {
+
+                    if (i < V - 1 && (edgesname[j] == path[i].ToString() + path[i + 1].ToString() || edgesname[j] == path[i+1].ToString() + path[i].ToString()))
+                    {
+                        Console.WriteLine(edgesname[j]);
+                        //addEdges(items[path[i]].Location.X, items[path[i]].Location.Y, items[path[i + 1]].Location.X, items[path[i+1]].Location.Y,path[i].ToString(),path[i+1].ToString());
+                        g = CreateGraphics();
+                        Pen a = new Pen(Color.Red, 5);
+                        g.DrawLine(a, items[path[i]].Location.X, items[path[i]].Location.Y, items[path[i + 1]].Location.X, items[path[i + 1]].Location.Y);
+                       
+                    }
+                }
+            }
+ 
+         
+          label1.Text += " " + path[0] + " ";
+          Pen b = new Pen(Color.Red, 5);
+          g.DrawLine(b, items[path[path.Length-1]].Location.X, items[path[path.Length-1]].Location.Y, items[path[0]].Location.X, items[path[0]].Location.Y);
+          g.Dispose();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if ( itemcount == V && itemcount != 0)
+                hamCycle(vertexmatris);
+            else
+                MessageBox.Show("Please add more vertex");
+        }
+
+        private void resetbutton_Click(object sender, EventArgs e)
+        {
+            for(int i = 0 ; i<itemcount ; i++)
+            {
+                items[i].Visible = false;
+            }
+
+            try
+            {
+                this.Invalidate();
+            }
+
+            catch(Exception exp)
+            {
+
+            }
+            itemcount = 0;
+            additem = 0;
+            firstvertexname = "";
+            label1.Text = "";
+            edgestack=0;
+            edgescount = 0;
+            vertexcountText.Text = "";
+            V = 0;
+        }
+
+       
+    }
+}
